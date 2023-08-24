058da1b6d3622fbb457ae358e95cb2f551f38511
Vuong Nguyen <nguvy062@mymail.unisa.edu.au>
2023-08-24 14:14:47 +0930

    updated

--- COMP1046/Week5/Task4.py
+++ COMP1046/Week5/Task4.py
@@ -1,3 +1,31 @@
+#Task1
+class Product:
+    count = 0
+    totalPrice = 0.0
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        Product.count += 1
+        Product.totalPrice += price
+
+    @classmethod
+    def averagePrice(cls):
+        if cls.count == 0:
+            return 0.0
+        return cls.totalPrice / cls.count
+
+# Test cases
+Product("Pen", 3.0)
+Product("Notebook", 5.0)
+Product("Eraser", 1.5)
+Product("Pencil", 2.5)
+
+print("Average product price:", Product.averagePrice())
+
+
+
+#Task 2-4
 from abc import ABC, abstractmethod
 
 class Shape(ABC):
