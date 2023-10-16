d5b4fcd5b03b4f40b1142e78d55e197842fe060a
Vuong Nguyen <nguvy062@mymail.unisa.edu.au>
2023-10-16 14:59:26 +1030

    updated

--- COMP1046/Week11/Workshop11.py
+++ COMP1046/Week11/Workshop11.py
@@ -1,81 +1,131 @@
 from abc import ABC, abstractmethod
+
+class InvalidLevelError(Exception):
+    pass
+
+class InvalidCategoryError(Exception):
+    pass
+
 class Employee(ABC):
+
+    # No potential error
     def __init__(self, firstName, lastName, employeeID):
         self.__firstName = firstName
         self.__lastName = lastName
         self.__employeeID = employeeID
         self.salary = 0
+
+    # No potential error
     @abstractmethod
     def setSalary(self, salary):
         pass
-    
+
+    # No potential error
     def getFirstName(self):
         return self.__firstName
-    
+
+    # No potential error
     def getLastName(self):
         return self.__lastName
-    
+
+    # No potential error
     def getEmployeeID(self):
         return self.__employeeID
-    
+
+    # No potential error
     @abstractmethod
     def getInformation(self):
         pass
 
+
 class TeachingStaff(Employee):
+
     def __init__(self, firstName, lastName, employeeID, teachingArea, category):
         super().__init__(firstName, lastName, employeeID)
         self.teachingArea = teachingArea
+
+        # Potential type error
         self.category = 0
-        #Potential TypeError if category is not an integer
         try:
-            if 1<= category <= 5:
+            if category >= 1 and category <= 5:
                 self.category = category
             else:
-                print("Error: The category of a teaching staff must be between 1 and 5.")
+                raise InvalidCategoryError("The category of a teaching staff must be between 1 and 5.")
         except TypeError:
-            print("Type Error: The category attribute must be a number")    
-    
+            print("Type Error: The category attribute must be a number.")
+
+    # Potential type error
     def setSalary(self, newSalary):
-        self.salary += ((self.category *10) + newSalary)
-    
-    def getInformation(self): 
-        try: 
-            info = "-Staff Information-\nID: " + self.getEmployeeID() + "\nName: " + self.getFirstName() + \
-        " " + self.getLastName() + "nArea of Expertise: " + self.teachingArea + "\nCategory: " \
-+ self.category + "\nSalary: " + self.salary
-            print(info) 
-        except TypeError : 
-            return "Type Error: One of the Teaching Staff attributes is not a string."
-        #potential type error : this line may rise a TypeError when trying to perform arithmetic operations
+        try:
+            self.salary += ((self.category * 10) + newSalary)
+        except TypeError:
+            print('Type Error: The salary argument must be a number.')
+
+    # Potential type error at self.getEmployeeID() and self.category, potential syntax error
+    def getInformation(self):
+        try:
+            return "-Staff Information-\nID: " + self.getEmployeeID() + "\nName: " + self.getFirstName() + \
+            " " + self.getLastName() + "nArea of Expertise: " + self.teachingArea + "\nCategory: " \
+                + self.category + "\nSalary: " + self.salary
+        except TypeError:
+            return 'Type Error: One of the Teaching Staff attributes is not a string. (All attributes must be converted into strings when using concatenation)'
 
 
 class AdministrativeStaff(Employee):
+
     def __init__(self, firstName, lastName, employeeID, level):
         super().__init__(firstName, lastName, employeeID)
+
         self.level = 0
-        #Potential TypeError if level is not an integer
-        if level >= 1 and level <= 3:
-            self.level = level
-        else:
-            print("Error: The level of an administrative staff must be between 1 and 3.")
-    
+
+        # Potential type error
+        try:
+            if level >= 1 and level <= 3:
+                self.level = level
+            else:
+                raise InvalidLevelError("The level of an administrative staff must be between 1 and 3.")
+        except TypeError:
+            print('Type Error: The level attribute must be a number.')
+
+    # Potential type error and attribute error
     def setSalary(self, newSalary):
-        self.salary += ((self.level * 15) + newSalary)
-        #potential type error : this line may rise a TypeError when trying to perform arithmetic operations
+        try:
+            self.salary += ((self.level * 15) + newSalary)
+        except TypeError:
+            print('Type Error: The salary argument must be a number.')
 
-
+    # Potential type error at self.getEmployeeID() and self.level
     def getInformation(self):
-        #Potential TypeError: this line may rise a TypeError if salary is not a string, or level is not a string
-        return "-Staff Information-\nID: " + self.getEmployeeID() + "\nName: " + self.getFirstName() + \
-" " + self.getLastName() + "\nLevel:" + self.level + "\nSalary:" + self.salary
+        try:
+            return "-Staff Information-\nID: " + self.getEmployeeID() + "\nName: " + self.getFirstName() + \
+                " " + self.getLastName() + "\nLevel:" + self.level + "\nSalary:" + self.salary
+        except TypeError:
+            return "Type Error: One of the Administrative Staff attributes is not a string. (All attributes must be converted into strings when using concatenation)"
 
 
-teacherA = TeachingStaff(1, "Dallas", 991074, "Law", "4")
-teacherA.setSalary(30000)
-print(teacherA.getInformation())
-print("This message should appear on your screen.")
-teacherB = TeachingStaff("Leeloo", "Dallas", 10095912, "English", 5)
-teacherB.setSalary(22500)
-print(teacherB.getInformation())
-print("This message should appear if exceptions are all handled correctly.")
+# Testing Part
+
+# teacherA = TeachingStaff(1, "Dallas", 991074, "Law", "4") 
+# teacherA.setSalary(30000) 
+# print(teacherA.getInformation())
+# print("This message should appear on your screen.") 
+# teacherB = TeachingStaff("Leeloo", "Dallas", 10095912, "English", 5) 
+# teacherB.setSalary(22500) 
+# print(teacherB.getInformation())
+# print("This message should appear if exceptions are all handled correctly.")
+
+try:
+    teacherB = TeachingStaff("Leeloo", "Dallas", 10095912, "English", 11) 
+    teacherB.setSalary(22500) 
+    print(teacherB.getInformation())
+    print("This line should not be printed on the screen if exception is not handled.")
+except InvalidCategoryError:
+    print("Error: The category of a teaching staff must be between 1 and 5.")
+
+try:
+    adminB = AdministrativeStaff("Ruby", "Rhod", "10095518", -1) 
+    print(adminB.getInformation())
+    adminB.setSalary(11100) 
+    print("This line should also not be printed on the screen if exception is not handled.") 
+except InvalidLevelError:
+    print("Error: The level of an administrative staff must be between 1 and 3.")
