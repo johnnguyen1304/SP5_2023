c53a204d07fb513b9118e7717a187fa07843dd84
Vuong Nguyen <nguvy062@mymail.unisa.edu.au>
2023-10-26 14:24:53 +1030

    updated

--- COMP1046/Week12/temperature.py
+++ COMP1046/Week12/temperature.py
@@ -1,6 +1,6 @@
 class Temperature:
     def __init__(self, degree):
-        if not isinstance(degree, (int, float)):
+        if not isinstance(degree, (int, float)) or isinstance(degree,bool):
             raise TypeError("Degree must be a numeric value (int or float).")
         # Initialize the private attribute degree
         self.__degree = degree
@@ -13,8 +13,3 @@ class Temperature:
     def get_fahrenheit(self):
         fahrenheit = (self.__degree * 9/5) + 32
         return fahrenheit
-
-# Example usage:
-# temperature_instance = Temperature(25)
-# celsius_temp = temperature_instance.get_celsius()
-# print(f"Temperature in Celsius: {celsius_temp}")
